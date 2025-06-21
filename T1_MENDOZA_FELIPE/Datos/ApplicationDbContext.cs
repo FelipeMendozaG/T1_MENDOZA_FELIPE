11a26c8a8d44ee6bf01a3855953a58ceff24b28d
upn_i <upn_i@DESKTOP-1KG2GS2>
2025-06-22 01:35:20 +0200

    first commit

---
+++ T1_MENDOZA_FELIPE/Datos/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using T1_MENDOZA_FELIPE.Models;
+
+namespace T1_MENDOZA_FELIPE.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Libro> Libros { get; set; }
+
+
+    }
+}
---
+++ T1_MENDOZA_FELIPE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace T1_MENDOZA_FELIPE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
